a32cac4ebf5e6eba5e19472deec458937bd88a00
Mridul Vashistha <mridul.vashistha@saviant.local>
2021-09-01 16:30:12 +0530

    second Commit

--- src/test/java/com/missiongroup/qa/testcases/LoginPageTest.java
+++ src/test/java/com/missiongroup/qa/testcases/LoginPageTest.java
@@ -28,7 +28,7 @@ public class LoginPageTest extends TestBase{
 	@Test(priority=1)
 	public void loginPageTitleTest(){
 		String title = loginPage.validateLoginPageTitle();
-		Assert.assertEquals(title, "Mission Group Homes");
+		Assert.assertEquals(title, "Mission Group Homes Test");
 	}
 	
 	/*@Test(priority=2)
